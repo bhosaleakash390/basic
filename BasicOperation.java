f0255abe0a394918d1e38e691313718fe104798c
bhosaleakash390 <bhosaleakash390@gmailcom>
2025-05-07 15:41:29 +0530

    Initial commit1

---
+++ BasicOperation.java
@@ -0,0 +1,14 @@
+public class BasicOperation {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int diff = a - b;   
+        int product = a * b;
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+
+        System.out.println("Multiplication: " + product);
+    }
+}
---
+++ Division.java
@@ -0,0 +1,10 @@
+public class Division {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 20;
+
+        double quotient = a / b;
+
+        System.out.println("division: " + quotient);
+    }
+}
